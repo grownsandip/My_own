7b653b5e938f07452c0749c0ba50f4164764057c
Sandip <roysandip33290@gmail.com>
2024-09-02 18:00:18 +0530

    corrected some bugs

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='mb-10 px-4 text-center text-gray-500'>
-        <small className='mb-2 block text-xs'>&copy; 2023 Sandip.All Rights Reserved</small>
-        <p className='text-xs'><span className='font-semibold'>About this website</span>built with top notch tech stack ,also demonstrates skills (React,TypeScript,NextJs) & vercel for hosting</p>
+        <small className='mb-2 block text-xs'>&copy; 2023 Sandip Roy.All Rights Reserved</small>
+        <p className='text-xs'><span className='font-semibold'>About this website </span>built with top notch tech stack ,also demonstrates skills (React,TypeScript,NextJs) & vercel for hosting</p>
     </footer>
   )
 }
